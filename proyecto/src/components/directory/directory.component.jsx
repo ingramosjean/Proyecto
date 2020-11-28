7fd73cc374a0ee49b1ae06001676d1d1b1471a18
Jean Pierre <ingramosjean@gmail.com>
2020-11-28 09:05:59 -0500

    solodescarga

--- proyecto/src/components/directory/directory.component.jsx
+++ proyecto/src/components/directory/directory.component.jsx
@@ -1,38 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import MenuItem from '../menu-item/menu-item.component';
 
+import DirectoryContext from '../../contexts/directory/directory.context';
+
 import './directory.styles.scss';
 
-class Directory extends React.Component {
-  constructor() {
-    super();
+const Directory = () => {
+  const sections = useContext(DirectoryContext);
 
-    this.state = {
-      sections: [
-        {
-          title: 'BICICLETAS',
-          imageUrl: 'https://assets.entrepreneur.com/content/3x2/2000/20160419164404-Depositphotos-32428429-l-2015.jpeg',
-          id: 1
-        },
-        {
-          title: 'TOMATODOS',
-          imageUrl: 'https://www.graficmerch.com/gfmrhwp/wp-content/uploads/2018/10/TOMATODO-CON-METAL-1.jpg',
-          id: 2
-        }
-      ]
-    };
-  }
-
-  render() {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className='directory-menu'>
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
 
 export default Directory;
---
+++ proyecto/src/pages/shop/shop.data.js
@@ -0,0 +1,60 @@
+const SHOP_DATA = [
+    {
+        id: 1,
+        title: 'Bicicleta',
+        routeName: 'bike',
+        items: [
+          {
+            id: 11,
+            name: 'Youma Gris',
+            imageUrl: 'https://i.ibb.co/nb8bc6Z/Whats-App-Image-2020-10-29-at-11-48-33-1.jpg',
+            price: 700
+          },
+          {
+            id: 12,
+            name: 'Youma Blanco',
+            imageUrl: 'https://i.ibb.co/cYSzj6Q/Whats-App-Image-2020-10-29-at-11-48-34.jpg',
+            price: 700
+          },
+          {
+            id: 13,
+            name: 'Youma Gris 5 aspas',
+            imageUrl: 'https://i.ibb.co/qWY3z5q/Whats-App-Image-2020-10-29-at-11-48-24-1.jpg',
+            price: 700
+          },
+          {
+            id: 14,
+            name: 'Youma Negro con Rojo',
+            imageUrl: 'https://i.ibb.co/ZzKcPwZ/Whats-App-Image-2020-10-29-at-11-48-24.jpg',
+            price: 700
+          }
+        ]
+    },
+    {
+        id: 2,
+        title: 'Tomatodo',
+        routeName: 'tomatodo',
+        items: [
+          {
+            id: 21,
+            name: 'Tomatodo Sandia',
+            imageUrl: 'https://i.ibb.co/k8xqVLg/Whats-App-Image-2020-10-29-at-11-48-25.jpg',
+            price: 50
+          },
+          {
+            id: 22,
+            name: 'Tomatodo Chocolate',
+            imageUrl: 'https://i.ibb.co/7zmR3HZ/Whats-App-Image-2020-10-29-at-11-48-33.jpg',
+            price: 50
+          },
+          {
+            id: 23,
+            name: 'Tomatodo Cara Feliz',
+            imageUrl: 'https://i.ibb.co/B384s9s/Whats-App-Image-2020-10-29-at-11-48-34-1.jpg',
+            price: 50
+          }
+        ]
+    }
+]
+
+export default SHOP_DATA;
